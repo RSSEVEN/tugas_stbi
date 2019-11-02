2b6828e82ec18b036858ca68742737d901f8bb73
RSSEVEN <48554708+RSSEVEN@users.noreply.github.com>
2019-11-02 23:51:09 +0700

    Add files via upload

---
+++ kelompok.php
@@ -0,0 +1,15 @@
+<div class="container pt">
+		<div class="row mt">
+			<div class="col-lg-6 col-lg-offset-3 centered">
+				<h3>Disusun Oleh</h3>
+				<hr>
+			<table class='table table-stripped'>
+				<tr>
+					<td>Nama</td>
+					<td>:</td>
+					<td>Rizky Syahrul Ramadhan (16.01.53.0122)</td>
+				</tr>
+			</table>
+			</div>
+		</div>
+	</div>
